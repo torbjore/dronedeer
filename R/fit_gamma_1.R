5f1b76a08ff490e8da84ba3ce3af3fa6efca2778
Torbjørn Ergon <torbjore@uio.no>
2023-12-04 15:58:18 -0500

    Before sending to Daniel

--- R/fit_gamma_1.R
+++ R/fit_gamma_1.R
@@ -26,12 +26,12 @@ N[is.na(N)] <- 0
 nrowY <- nrow(LDDdata$data$Y)
 ncolY <- ncol(LDDdata$data$Y)
 
-Inits = function(){
-  rate = runif(1, 0.5, 5)
-  p1 = exp(log(p1hat)*runif(1, 0.9, 1.1))
-  p2 = exp(log(p2hat)*runif(1, 0.9, 1.1))
+Inits <- function(){
+  rate <- runif(1, 0.1, 1)
+  p1 <- exp(log(p1hat)*runif(1, 0.9, 1.1))
+  p2 <- exp(log(p2hat)*runif(1, 0.9, 1.1))
   list(
-#    mu0 = log(lambdahat*runif(length(lambdahat), 0.9, 1.1)),
+    #mu0 = log(lambdahat*runif(length(lambdahat), 0.9, 1.1)),
     exp_mu0 = lambdahat*runif(length(lambdahat), 0.9, 1.1),
     logit_p1 = rnorm(prior_parameters_for_p$mu_logit_p, prior_parameters_for_p$sigma_logit_p/5),
     logit_p2 = rnorm(prior_parameters_for_p$mu_logit_p, prior_parameters_for_p$sigma_logit_p/5),
@@ -45,9 +45,7 @@ Inits = function(){
 # SETTING UP THE MCMC
 DoubleObsMultisiteModel <- nimbleModel(
   nimbleCode_DOMM_gamma_1,
-  constants = list(lamblow = 0.1*lambdahat,  # 0.1 to 10 times point estimate
-                   lambupp = 10*lambdahat,
-                   N_surv = length(LDDdata$const$N_sites),
+  constants = list(N_surv = length(LDDdata$const$N_sites),
                    N_sites = LDDdata$const$N_sites,
                    mu_logit_p = prior_parameters_for_p$mu_logit_p,
                    sigma_logit_p = prior_parameters_for_p$sigma_logit_p,
@@ -60,31 +58,49 @@ DoubleObsMultisiteModel <- nimbleModel(
   inits = Inits()
 )
 
+# dataNodes <- DoubleObsMultisiteModel$getNodeNames(dataOnly = TRUE)
+# parentNodes <- DoubleObsMultisiteModel$getParents(dataNodes, stochOnly = TRUE, includeData = FALSE, upstream = TRUE)
+# simNodes <- DoubleObsMultisiteModel$getDependencies(parentNodes, self = FALSE)
+
 t1 <- Sys.time()
 CDoubleObsMultisiteModel <- compileNimble(DoubleObsMultisiteModel) # Needs to be compiled for the last step
-#DoubleObsMultisiteConf <- configureMCMC(DoubleObsMultisiteModel, monitors = c("mean_lambda" , "p1", "p2", "mu0", "rate", "beta"), enableWAIC = TRUE)
-DoubleObsMultisiteConf <- configureMCMC(DoubleObsMultisiteModel, monitors = c("p1", "p2", "exp_mu0", "rate", "beta"), enableWAIC = TRUE)
+DoubleObsMultisiteConf <- configureMCMC(DoubleObsMultisiteModel, 
+                                        monitors = c("p1", "p2", "mu0", "rate", "beta", "Disc_Y", "Disc_New_Y"), 
+                                        #monitors = c("p1", "p2", "mu0", "rate", "beta"), 
+                                        enableWAIC = TRUE)
 
-# Trying to set up a block sampler for 'rate', 'mu0[1:8]' nodes
-nn <- DoubleObsMultisiteModel$expandNodeNames(c('rate', 'beta', 'mu0'))
-DoubleObsMultisiteConf$removeSamplers(nn)
-DoubleObsMultisiteConf$addSampler(nn, 'RW_block', control = list(adaptScaleOnly=FALSE))
+# Setting up a block sampler (I've tried various combiations of blocking, chains often get stuck with any combination)
+nn1 <- DoubleObsMultisiteModel$expandNodeNames(c('beta', 'rate')) #
+DoubleObsMultisiteConf$removeSamplers(nn1)
+DoubleObsMultisiteConf$addSampler(nn1, 'RW_block', control = list(adaptScaleOnly=FALSE))
+nn2 <- DoubleObsMultisiteModel$expandNodeNames(c('exp_mu0')) #
+DoubleObsMultisiteConf$removeSamplers(nn2)
+DoubleObsMultisiteConf$addSampler(nn2, 'RW_block', control = list(adaptScaleOnly=FALSE))
+
+print(DoubleObsMultisiteConf)
 
 DoubleObsMultisiteMCMC <- buildMCMC(DoubleObsMultisiteConf)
 CDoubleObsMultisiteMCMC <- compileNimble(DoubleObsMultisiteMCMC)
+
+# DoubleObsMultisiteConf$getMonitors()
+# DoubleObsMultisiteConf$getSamplerDefinition('beta')
+# DoubleObsMultisiteConf$getSamplers('beta')
+# DoubleObsMultisiteConf$printSamplers('exp_mu0')
+
 
 t2 <- Sys.time()
 cat("Compilation time:")
 t2-t1
 
 t3 <- Sys.time()
+init.values <- list(Inits(), Inits(), Inits())
 posterior_gamma_1 <- runMCMC(
   CDoubleObsMultisiteMCMC,
-  niter= 20000, #0,
-  nburnin= 5000,
-  nchain=3,
-  thin=4,
-  inits = Inits,
+  niter = 10000, #115000,
+  nburnin = 5000, #15000,
+  nchain = 3,
+  thin = 4,
+  inits = init.values,
   samplesAsCodaMCMC = TRUE,
   WAIC = TRUE)
 t4 <- Sys.time()
@@ -92,8 +108,13 @@ t4 <- Sys.time()
 cat("Run time:")
 t4-t3
 
-plot(posterior_gamma_1$samples)
+plot(posterior_gamma_1$samples) # 1 = svart, 2 = rød, 3 = grønn
 summary(posterior_gamma_1$samples)
+gelman.diag(posterior_gamma_1$samples)
+
+lapply(posterior_gamma_1$samples, function(i) var(i[,"rate"]))
+lapply(posterior_gamma_1$samples, function(i) mean(i[,"rate"]))
+lapply(init.values, function(i) i[c("exp_mu0", "rate", "beta")])
 
 posterior_gamma_1$WAIC
 
@@ -104,6 +125,14 @@ posterior_gamma_1$WAIC
 # [1] 117.1465
 #   
 
-gelman.diag(posterior_gamma_1$samples)
 
 #save(posterior_gamma_1, file = "data/posterior_samples/gamma_1.RData")
+
+# # Posterior predictive checks
+# 
+# samp <- as.matrix(posterior_gamma_1$samples)
+# plot(samp[,"Disc_New_Y"] ~ samp[,"Disc_Y"])
+# abline(0,1, col="red")
+# 
+# mean(samp[,"Disc_New_Y"] > samp[,"Disc_Y"])
+
--- R/nimble_models/nimbleCode_DOMM_gamma_1.q
+++ R/nimble_models/nimbleCode_DOMM_gamma_1.q
@@ -1,4 +1,4 @@
-# Double observer multi-site model with lognormal lambda and first order covariate effect
+# Double observer multi-site model with gamma distributed lambda and first order covariate effect
 
 # DEFINING THE MODEL
 nimbleCode_DOMM_gamma_1 <- nimbleCode({
@@ -23,10 +23,11 @@ nimbleCode_DOMM_gamma_1 <- nimbleCode({
   }
   
   # Priors
-  rate ~ dunif(0,10)
+  rate ~ dunif(0, 10)
 
   for(k in 1:N_sam){
     exp_mu0[k] ~ dunif(0, 10)
+    mu0[k] <- log(exp_mu0[k])
   }
 
   beta ~ dnorm(0, sd=2)
@@ -35,4 +36,23 @@ nimbleCode_DOMM_gamma_1 <- nimbleCode({
   logit_p2 ~ dnorm(mu_logit_p, sd = sigma_logit_p)
   p1 <- exp(logit_p1)/(1+exp(logit_p1))
   p2 <- exp(logit_p2)/(1+exp(logit_p2))
+  
+  # For posterior predictive checks
+  for(s in 1:N_surv){
+    for(i in 1:N_sites[s]){
+      New_Y[s,i] ~ dbin(Psum, N[s,i])
+      #New_y[s,i,1:3] ~ dmulti(pi[1:3], Y[s,i])
+      E_Y[s,i] <- Psum*N[s,i] + 0.0001
+      #E_y[s,i,1:3] <- pi[1:3]*Y[s,i]
+      # Discrepancy contributions
+      DiscC_Y[s,i] <- pow(Y[s,i] - E_Y[s,i], 2) / E_Y[s,i]
+      DiscC_New_Y[s,i] <- pow(New_Y[s,i] - E_Y[s,i], 2) / E_Y[s,i]
+    }
+    # Sums per survey
+    Disc_Y_s[s] <- sum(DiscC_Y[s, 1:N_sites[s]])
+    Disc_New_Y_s[s] <- sum(DiscC_New_Y[s, 1:N_sites[s]])
+  }
+  # Discrepancy statistics (Pearson chi-sq, but note many small expectations)
+  Disc_Y <- sum(Disc_Y_s[1:N_surv])
+  Disc_New_Y <- sum(Disc_New_Y_s[1:N_surv])
 })
