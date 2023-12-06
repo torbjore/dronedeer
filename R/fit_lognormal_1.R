64bc3930befb5624ac53baac0a2d2f9b870fcbb4
Torbjørn Ergon <torbjore@uio.no>
2023-12-05 20:40:39 -0500

    Complete random p for log-normal model

--- R/fit_lognormal_1.R
+++ R/fit_lognormal_1.R
@@ -29,16 +29,22 @@ ncolY <- ncol(LDDdata$data$Y)
 # FUNCTION FOR INITIAL VALUES
 Inits <- function(){
   sigma <- runif(1, 0.5, 1)
-#  sigma <- runif(1, 0.1, 0.3)
+  #  sigma <- runif(1, 0.1, 0.3)
+  sigma_p <- runif(1, 0.4, 0.9)
   p1 <- exp(log(p1hat)*runif(1, 0.9, 1.1))
   p2 <- exp(log(p2hat)*runif(1, 0.9, 1.1))
+  mu_p1 <- log(p1/(1-p1))
+  mu_p2 <- log(p1/(1-p1))
   list(
     mu0 = log(lambdahat*runif(length(lambdahat), 0.9, 1.1)),
     epsilon = matrix(rnorm(nrowY*ncolY, 0, sigma) , nrow = nrowY, ncol = ncolY),
-    logit_p1 = rnorm(prior_parameters_for_p$mu_logit_p, prior_parameters_for_p$sigma_logit_p/5),
-    logit_p2 = rnorm(prior_parameters_for_p$mu_logit_p, prior_parameters_for_p$sigma_logit_p/5),
+    mu_p1 = mu_p1,
+    mu_p2 = mu_p2,
+    logit_p1 = matrix(rnorm(nrowY*ncolY, mu_p1, sigma_p/2) , nrow = nrowY, ncol = ncolY),
+    logit_p2 = matrix(rnorm(nrowY*ncolY, mu_p2, sigma_p/2) , nrow = nrowY, ncol = ncolY),
     N = N,
     sigma = sigma,
+    sigma_p = sigma_p,
     beta = runif(1, -0.5, 0.5)
   )
 }
@@ -50,8 +56,8 @@ DoubleObsMultisiteModel <- nimbleModel(
                    lambupp = 10*lambdahat,
                    N_surv = length(LDDdata$const$N_sites),
                    N_sites = LDDdata$const$N_sites,
-                   mu_logit_p = prior_parameters_for_p$mu_logit_p,
-                   sigma_logit_p = prior_parameters_for_p$sigma_logit_p,
+                   prior_mu_logit_p = prior_parameters_for_p$mu_logit_p,
+                   prior_sigma_logit_p = prior_parameters_for_p$sigma_logit_p,
                    sam = LDDdata$const$sam,
                    N_sam = length(unique(LDDdata$const$sam)),
                    area = LDDdata$const$area,
@@ -66,7 +72,8 @@ CDoubleObsMultisiteModel <- compileNimble(DoubleObsMultisiteModel) # Needs to be
 DoubleObsMultisiteConf <- configureMCMC(DoubleObsMultisiteModel, 
 #                                        monitors = c("p1", "p2", "mu0", "sigma", "beta"), enableWAIC = TRUE)
 #                                        monitors = c("median_lambda", "mean_lambda" , "p1", "p2", "mu0", "sigma", "beta"), enableWAIC = TRUE)
-                                        monitors = c("Disc_New_Y", "Disc_Y", "Disc_New_y", "Disc_y", "median_lambda", "mean_lambda" , "p1", "p2", "mu0", "sigma", "beta"), enableWAIC = TRUE)
+#                                        monitors = c("Disc_New_Y", "Disc_Y", "Disc_New_y", "Disc_y", "median_lambda", "mean_lambda" , "mu_p1", "mu_p2", "mu0", "sigma", "sigma_p", "beta"), enableWAIC = TRUE)
+                                        monitors = c("Disc_New_Y", "Disc_Y", "Disc_New_y", "Disc_y", "mu_p1", "mu_p2", "mu0", "sigma", "sigma_p", "beta"), enableWAIC = TRUE)
 
 
 DoubleObsMultisiteMCMC <- buildMCMC(DoubleObsMultisiteConf)
@@ -117,16 +124,28 @@ mean(samp[, "Disc_New_Y"] > samp[, "Disc_Y"])
 plot(samp[,"Disc_New_y"] ~ samp[,"Disc_y"])
 abline(0, 1, col="red")
 mean(samp[, "Disc_New_y"] > samp[, "Disc_y"])
+# Now OK
 # Before adding random effects: [1] 0.03423333. This means that there are more 
 # variation in y[] than expected - probably because observers vary in focus from
 # picture to picture
-
-# Looking at lack of fit
-n <- sum(colsumy)
-# Expected ab
-n*p1hat*(1-p2hat)
-# Expected jb
-n*p2hat*(1-p1hat)
-# Expected both
-n*p2hat*p1hat
-# I don't think this says much though
+# 
+# 2. Quantiles for each variable:
+# 
+# 2.5%     25%     50%     75%    97.5%
+# Disc_New_Y  0.14707  0.7933  1.4121  2.1963   4.1576
+# Disc_New_y 25.60289 38.2987 47.8034 60.2280 103.6541
+# Disc_Y      0.14868  1.0016  1.6883  2.6257   4.9057
+# Disc_y     28.16323 41.1363 50.7363 63.4592 107.5061
+# beta        0.04675  0.3554  0.5168  0.6756   0.9764
+# mu0[1]     -2.38229 -1.9036 -1.5404 -1.1618  -0.4973
+# mu0[2]     -2.81964 -2.4402 -2.1251 -1.7892  -1.1899
+# mu0[3]     -6.80932 -6.0006 -5.0581 -3.9832  -2.5831
+# mu0[4]     -6.83190 -6.1345 -5.2787 -4.2994  -2.7585
+# mu0[5]     -4.21993 -3.8896 -3.5496 -3.1393  -2.3198
+# mu0[6]     -3.31674 -2.9519 -2.6638 -2.3719  -1.8336
+# mu0[7]     -6.84318 -6.2259 -5.4742 -4.5561  -2.9776
+# mu0[8]     -4.54373 -4.2160 -3.8433 -3.3806  -2.4791
+# mu_p1       0.63152  1.0096  1.2063  1.3976   1.7900
+# mu_p2       1.08607  1.4835  1.6970  1.9202   2.3410
+# sigma       1.31758  1.5985  1.7541  1.9110   2.2320
+# sigma_p     0.69672  1.0833  1.2651  1.3916   1.4892
--- R/nimble_models/nimbleCode_DOMM_lognormal_1.q
+++ R/nimble_models/nimbleCode_DOMM_lognormal_1.q
@@ -3,21 +3,35 @@
 # DEFINING THE MODEL
 nimbleCode_DOMM_lognormal_1 <- nimbleCode({
   # Model
-  Psum <- 1-(1-p1)*(1-p2)
-  pi[1] <- p1*(1-p2)/Psum
-  pi[2] <- (1-p1)*p2/Psum
-  pi[3] <- p1*p2/Psum
+  
+  # Fixed p's
+  # Psum <- 1-(1-p1)*(1-p2)
+  # pi[1] <- p1*(1-p2)/Psum
+  # pi[2] <- (1-p1)*p2/Psum
+  # pi[3] <- p1*p2/Psum
   
   for(s in 1:N_surv){
     for(i in 1:N_sites[s]){
+      # Random p's
+      logit_p1[s,i] ~ dnorm(mu_p1, sd = sigma_p)
+      logit_p2[s,i] ~ dnorm(mu_p2, sd = sigma_p)
+      p1[s,i] <- exp(logit_p1[s,i])/(1+exp(logit_p1[s,i]))
+      p2[s,i] <- exp(logit_p2[s,i])/(1+exp(logit_p2[s,i]))
+      
+      Psum[s,i] <- 1-(1-p1[s,i])*(1-p2[s,i])
+      pi[s,i,1] <- p1[s,i]*(1-p2[s,i])/Psum[s,i]
+      pi[s,i,2] <- (1-p1[s,i])*p2[s,i]/Psum[s,i]
+      pi[s,i,3] <- p1[s,i]*p2[s,i]/Psum[s,i]
+      
+      # Observation model:
+      Y[s,i] ~ dbin(Psum[s,i], N[s,i])
+      y[s, i, 1:3] ~ dmulti(pi[s, i, 1:3], Y[s,i])
+      
       # Process model:
       N[s,i] ~ dpois(lambda[s,i]*area[s,i])
       lambda[s,i] <- exp(mu[s,i] + epsilon[s,i])
       mu[s,i] <- mu0[sam[s]] + x[s,i]*beta
       epsilon[s,i] ~ dnorm(0, sd = sigma)
-      # Observation model:
-      Y[s,i] ~ dbin(Psum, N[s,i])
-      y[s, i, 1:3] ~ dmulti(pi[1:3], Y[s,i])
     }
   }
   
@@ -28,13 +42,21 @@ nimbleCode_DOMM_lognormal_1 <- nimbleCode({
   
   beta ~ dnorm(0, sd=2) # assume that x is standardized (x_st = (x-mean(x))/sd(x))
   
-  logit_p1 ~ dnorm(mu_logit_p, sd = sigma_logit_p) 
-  logit_p2 ~ dnorm(mu_logit_p, sd = sigma_logit_p)
-  p1 <- exp(logit_p1)/(1+exp(logit_p1))
-  p2 <- exp(logit_p2)/(1+exp(logit_p2))
+  mu_p1 ~ dnorm(prior_mu_logit_p, sd = prior_sigma_logit_p) 
+  mu_p2 ~ dnorm(prior_mu_logit_p, sd = prior_sigma_logit_p)
+  sigma_p ~ dunif(0.1, 0.59)
+  # It is not reasonable that sigma_p could be very high because observers did an
+  # honest attempt to locate deer at all sites, and we we don't believe that a
+  # very high proportion of sites with zero counts actually had deer present (it's
+  # more likely that most of these sites did not have deer in them). Assuming
+  # that the 97.% percentile site had a maximum of 10 times higher detection
+  # odds as the 2.5% percentile site, we used a uniform prior for sigma_p with 
+  # an upper bound of log(10)/(1.96*2) = 0.59. To avoid poor mixing when the 
+  # chains entered flatparts of the likelihood surface, we also set a lower
+  # bound of 0.1.
   
-  sigma ~ dunif(0.5, 3) # WAIC 122.1556
-# sigma ~ dunif(0, 0.5) # WAIC 131.1987, chains hit the roof, and GOF is worse (Bayesian p-value based on Pearson statistic for Y 0.2989667)
+  sigma ~ dunif(0.5, 3) # WAIC 122.1556 (before random effects on p)
+# sigma ~ dunif(0, 0.5) # WAIC 131.1987 (before random effects on p), chains hit the roof, and GOF is worse (Bayesian p-value based on Pearson statistic for Y 0.2989667)
   # If allowed to be too small, chains sometimes hover around smaller values
   # before jumping to the higher values (but not the other way it looks).
   # I interpret this to mean that there is a local minimum with low sigma.
@@ -48,14 +70,14 @@ nimbleCode_DOMM_lognormal_1 <- nimbleCode({
   # For posterior predictive checks
   for(s in 1:N_surv){
     for(i in 1:N_sites[s]){
-      New_Y[s,i] ~ dbin(Psum, N[s,i])
-      New_y[s,i,1:3] ~ dmulti(pi[1:3], Y[s,i])
-      E_Y[s,i] <- Psum*N[s,i] + 0.0001
+      New_Y[s,i] ~ dbin(Psum[s,i], N[s,i])
+      New_y[s,i,1:3] ~ dmulti(pi[s,i,1:3], Y[s,i])
+      E_Y[s,i] <- Psum[s,i]*N[s,i] + 0.0001
       # Discrepancy contributions
       DiscC_Y[s,i] <- (Y[s,i] - E_Y[s,i])^2 / E_Y[s,i]
       DiscC_New_Y[s,i] <- (New_Y[s,i] - E_Y[s,i])^2 / E_Y[s,i]
       for(j in 1:3){
-        E_y[s,i,j] <- pi[j]*Y[s,i] + 0.00001
+        E_y[s,i,j] <- pi[s,i,j]*Y[s,i] + 0.00001
         DiscC_y_v[s,i, j] <- (y[s,i,j] - E_y[s,i,j])^2 / E_y[s,i,j]
         DiscC_New_y_v[s,i, j] <- (New_y[s,i,j] - E_y[s,i,j])^2 / E_y[s,i,j]
       }
