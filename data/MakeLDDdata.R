a3f46c5ae7fdffaec7926f3d8001c20e200c9d4f
Torbjørn Ergon <torbjore@uio.no>
2024-01-23 13:28:43 -0500

    Adding lambdahat to LDDdata

--- data/MakeLDDdata.R
+++ data/MakeLDDdata.R
@@ -22,6 +22,33 @@ Counts$StudyArea_Month <- paste(Counts$Study_area, Counts$Month, sep="_")
 tab <- table(Counts$Survey,Counts$StudyArea_Month)
 sam <- apply(tab,1, function(i) which(i != 0))
 
+####################################
+# Simple estimates for prior means #
+####################################
+
+# Y per survey_area-and-month (sam)
+Y_per_sam <- apply(Y, 1, sum, na.rm=TRUE) |> tapply(INDEX = sam, sum)
+sam_names <- c("Haugen (April)", "Haugen (March)", "Raa (April)", "Raa (March)", "Søre Bjørkum (April)", "Søre Bjørkum (March)", "Sprakehaug (April)", "Sprakehaug (March)")
+names(Y_per_sam) <- sam_names
+
+# Simple estimate of detection probability
+colsumy <- apply(y, 3, sum, na.rm=TRUE)
+p1hat <- colsumy[3]/(colsumy[2]+colsumy[3])
+p2hat <- colsumy[3]/(colsumy[1]+colsumy[3])
+(phat_simple <- 1 - (1-p1hat)*(1-p2hat))
+
+# Area per sam
+area_per_sam <- apply(area, 1, sum, na.rm=TRUE) |> tapply(INDEX = sam, sum)
+(names(area_per_sam) <- sam_names)
+
+# Simple estimates of density
+lambdahat <- (Y_per_sam/phat_simple)/area_per_sam
+
+# For the sam's with zero detentions (see prior_zero_sites.R)
+lambdahat[3] <- 0.0066 # Raa (April)
+lambdahat[4] <- 0.0046 # Raa (March)
+lambdahat[7] <- 0.0047 # Sprakehaug (April)
+
 LDDdata <- list(
   data = list(
     y = y,
@@ -31,7 +58,8 @@ LDDdata <- list(
     N_sites = N_sites,
     area = area,
     mean_field_dist = mean_field_dist,
-    sam = sam # study area and month
+    sam = sam, # study area and month
+    lambdahat = lambdahat
   )
 )
 
