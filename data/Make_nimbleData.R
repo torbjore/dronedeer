21d003e147063a587ddf03c59d9143ded7bd4434
Torbjørn Ergon <torbjore@uio.no>
2025-04-02 09:09:16 +0200

    Updating data names

--- data/Make_nimbleData.R
+++ data/Make_nimbleData.R
@@ -6,20 +6,20 @@ load(file = "data/SiteData.rda")
 # Loading counts
 load(file = "data/CountData.rda")
 
-# KOMMENTAR: UseData inneholder også counts (men i et annet format)
+# KOMMENTAR: SiteData inneholder også counts (men i et annet format)
 
 # Number of sites for each survey
-N_sites <- table(UseData$Survey)
+N_sites <- table(SiteData$Survey)
 
 # Making y, Y, area and mean_field_dist 
-y <- tapply(Counts$Count, list(Counts$Survey, Counts$Site, Counts$Cat), sum) # 3D array rekke = survey, colonne = site (bilde), lag = Cat
+y <- tapply(CountData$Count, list(CountData$Survey, CountData$Site, CountData$Cat), sum) # 3D array rekke = survey, colonne = site (bilde), lag = Cat
 Y <- apply(y, c(1,2), sum) # Matrix with survey x no. deer
-area <- tapply(UseData$Area, list(UseData$Survey, UseData$Site), sum)/10000 # Area in hectare
-mean_field_dist <- tapply(UseData$mean_field_dist, list(UseData$Survey, UseData$Site), sum)
+area <- tapply(SiteData$Area, list(SiteData$Survey, SiteData$Site), sum)/10000 # Area in hectare
+mean_field_dist <- tapply(SiteData$mean_field_dist, list(SiteData$Survey, SiteData$Site), sum)
 
 # Making index for 'study-area and month' (sam)
-Counts$StudyArea_Month <- paste(Counts$Study_area, Counts$Month, sep="_")
-tab <- table(Counts$Survey,Counts$StudyArea_Month)
+CountData$StudyArea_Month <- paste(CountData$Study_area, CountData$Month, sep="_")
+tab <- table(CountData$Survey,CountData$StudyArea_Month)
 sam <- apply(tab,1, function(i) which(i != 0))
 
 ####################################
@@ -63,4 +63,4 @@ nimbleData <- list(
   )
 )
 
-save(nimbleData, file = "data/nimbleData.rda")
+#save(nimbleData, file = "data/nimbleData.rda")
